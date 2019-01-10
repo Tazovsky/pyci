62233409ab31d6838657fe8dd417101fe7304418
Kamil Foltynski <kamil@foltynski.com>
2019-01-10 01:56:00 +0100

    added pytest-datadir fixturer

--- tests/test_yaml.py
+++ tests/test_yaml.py
@@ -4,31 +4,24 @@ from pyci.yaml import make_custom_yaml
 from os.path import join as path
 import os
 
-def test_make_custom_yaml():
+def test_make_custom_yaml(shared_datadir):
 
     print(">>>>>> wd:" + str(os.path.dirname(os.path.realpath(__file__))))
     print("os.getcwd(): " + os.getcwd())
 
-    data_dir_path = "data"
-
     h1, yml1 = mkstemp()
-    ref_yml1 = path(data_dir_path, "testdata/application.yml")
-
-    print(os.listdir(path(data_dir_path, "testdata/")))
+    ref_yml1 = shared_datadir / "testdata/application.yml"
 
     make_custom_yaml(ref_yml1, "container-cmd", '["R", "-e print(1)"]', yml1)
 
-    assert filecmp.cmp(path(data_dir_path, "refdata/application1.yml"), yml1) == True
-
+    assert filecmp.cmp(shared_datadir / "refdata/application1.yml", yml1) == True
 
     h2, yml2 = mkstemp()
-    ref_yml2 = path(data_dir_path, "refdata/application1.yml")
-
-    print(os.listdir(ref_yml2))
+    ref_yml2 = shared_datadir / "refdata/application1.yml"
 
     make_custom_yaml(ref_yml2, "port", "8888", yml2)
 
-    assert filecmp.cmp(path(data_dir_path, "refdata/application2.yml"), yml2) == True
+    assert filecmp.cmp(shared_datadir / "refdata/application2.yml", yml2) == True
 
 # to recreate ref objects
 if False:
