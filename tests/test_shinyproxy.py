19ec04839cb96361287b3b172c876f04bd966cb5
Kamil Foltynski <kamil@foltynski.com>
2019-01-11 00:34:01 +0100

    updated unit tests

---
+++ tests/test_shinyproxy.py
@@ -0,0 +1,31 @@
+from tempfile import mkdtemp
+import os
+import hashlib
+from pyci.shinyproxy import deploy
+
+def test_deploy(shared_datadir):
+    json_path = shared_datadir / "testdata/config.json"
+    yaml_path = shared_datadir / "testdata/application.yml"
+    deployment_dir = mkdtemp()
+
+    output = deploy(json_path=json_path,
+                    yaml_path=yaml_path,
+                    deployment_dir=deployment_dir,
+                    deploy_cmd=None,
+                    url="https://www.shinyproxy.io/downloads/shinyproxy-2.0.5.jar",
+                    jar_name="shinyproxy.jar")
+
+    assert os.listdir(output["master"]["full_deployment_path"]) == ['application.yml', 'shinyproxy.jar']
+    assert os.listdir(output["user@somemail.com"]["full_deployment_path"]) == ['application.yml', 'shinyproxy.jar']
+
+    # check files hashes
+    with(open(os.path.join(output["master"]["full_deployment_path"], "application.yml"), "r")) as f:
+        f = f.read()
+
+    assert hashlib.md5(f.encode("utf-8")).hexdigest() == 'ec9ce72f8f4a1641173d6dcbff863050'
+
+    with(open(os.path.join(output["user@somemail.com"]["full_deployment_path"], "application.yml"), "r")) as f:
+        f = f.read()
+
+    assert hashlib.md5(f.encode("utf-8")).hexdigest() == '98753b036e2b06b2e161e0b2ad8db657'
+
