6ba94b4a8bc13d26e3963fdb4f76da63e688d8bb
Kamil Foltynski <kamil@foltynski.com>
2019-01-11 17:33:34 +0100

    fun to get template

--- pyci/utils.py
+++ pyci/utils.py
@@ -1,2 +1,9 @@
 import pkg_resources
+import shutil
+import os
+
 data_path = pkg_resources.resource_filename('pyci', 'data/')
+
+def get_deploy_py(dest: str = ".") -> None:
+
+    shutil.copy(os.path.join(data_path, "scripts", "deploy.py"), dest)
