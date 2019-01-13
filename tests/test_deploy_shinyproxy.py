9148aa2fb12539e51c15f5b2c03ec754234ec530
Kamil Foltynski <kamil@foltynski.com>
2019-01-13 13:01:21 +0100

    fixed selenium tests

--- tests/test_deploy_shinyproxy.py
+++ tests/test_deploy_shinyproxy.py
@@ -1,8 +1,10 @@
 from tempfile import mkdtemp
 import git
-import os
 from pyci.shinyproxy import *
-from pyci.utils import data_path
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.common.exceptions import NoSuchElementException
+import atexit
 
 def test_deploy_shinyproxy(shared_datadir):
     # working dir
@@ -19,16 +21,10 @@ def test_deploy_shinyproxy(shared_datadir):
 
     yaml_path = "application.yml"
     deployment_dir = os.path.join(dir_path, "deployed")
-    url = "https://www.shinyproxy.io/downloads/shinyproxy-2.0.5.jar"
-    jar_name = "shinyproxy.jar"
 
     if False:
         test_dir = "/Users/foltynsk/Projects/pyci/tests/data/testdata"
         json_path = os.path.join(test_dir, "test_deploy_config.json")
-        # json_dict = json.load(open(json_path, "r"))
-
-    # os.getcwd()
-    # '/Users/foltynsk/Projects/pyci'
 
     jar_name = "shinyproxy.jar"
     output = deploy(json_path=json_path,
@@ -39,5 +35,28 @@ def test_deploy_shinyproxy(shared_datadir):
                 jar_name=jar_name)
 
     assert isinstance(output["user1"]["process"].pid, int)
+    assert isinstance(output["user2"]["process"].pid, int)
+
+    atexit.register(output["user1"]["process"].kill)
+    atexit.register(output["user2"]["process"].kill)
+
+    # run selenium server
+    options = webdriver.ChromeOptions()
+    # headless option is set to not to open browser window
+    options.add_argument("headless")
+    driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=options)
+    atexit.register(driver.close)
+
+    # check if 1st instance is running
+    driver.get('http://localhost:{0}/login'.format("8081"))
+    # elem = driver.find_element_by_xpath("/html/body/div/div/form")
+    elem = driver.find_element_by_class_name("form-signin")
+    assert str(type(elem)) == "<class 'selenium.webdriver.remote.webelement.WebElement'>"
+
+    # check if 2nd instance is running
+    driver.get('http://localhost:{0}/login'.format("8082"))
+    # elem = driver.find_element_by_xpath("/html/body/div/div/form")
+    elem = driver.find_element_by_class_name("form-signin")
+    assert str(type(elem)) == "<class 'selenium.webdriver.remote.webelement.WebElement'>"
 
 
